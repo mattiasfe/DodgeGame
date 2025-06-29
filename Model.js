8411c8bf6ee451dcf4d0c3e1122c3c2abc72430d
mattiasfe <matferge@gmail.com>
2025-06-29 15:20:14 +0200

    GG

--- Model.js
+++ Model.js
@@ -1,26 +1,36 @@
 const Model = {
-    app:{
+    app: {
         currentCharacter: [],
-        curentBackground:  [],
-        currentDiffecuty: "",
-
+        currentBackground: [],  // Fixed typo (was 'curentBackground')
+        currentDifficulty: "",  // Fixed typo (was 'currentDiffeculty')
     },
 
-    input: {
-        characterCartman: {
-            Name: "Cartman",
-            imgRight: "CartmanRight.png",
-            imgLeft: "CartmanLeft.png",
-            Wepond: "Gun",
-            Health: "3",
-            Jump: "1"
-        }
-    },
+    input: {},
 
-    data:{
+    data: {
         board: {
-            height: 776,
-            width: 1040,
-        }
+            height: 670,
+            width: 1190
+        },
+        
+        character: [
+            {
+                name: "Cartman",
+                cartmanWidth: 96,
+                cartmanHeight: 96,
+                // These will need to be calculated after the object is created
+                cartmanX: 0,  // Placeholder - will be set later
+                cartmanY: 0,   // Placeholder - will be set later
+                imgRight: "CartmanRight.png",
+                x: 0,          // Placeholder - will reference cartmanX
+                y: 0           // Placeholder - will reference cartmanY
+            }
+        ]
     }
-}
+};
+
+// Now calculate the positions after the object exists
+Model.data.character[0].cartmanX = Model.data.board.width/2 - Model.data.character[0].cartmanWidth/2;
+Model.data.character[0].cartmanY = Model.data.board.height*7/8 - Model.data.character[0].cartmanHeight;
+Model.data.character[0].x = Model.data.character[0].cartmanX;
+Model.data.character[0].y = Model.data.character[0].cartmanY;
--- mainView.js
+++ mainView.js
@@ -1,11 +1,30 @@
 let board;
 let context;
 
-    function showBackground() {
+function showBackground() {
     board = document.getElementById("board");
     board.height = Model.data.board.height;
     board.width = Model.data.board.width;
     context = board.getContext("2d");
+    showCharacter();
 }
 
-
+function showCharacter() {
+    
+      // Fixed typo in function name (was "showCaracter")
+    let cartmanRight = new Image();  // Create the image first
+    cartmanRight.src = "CartmanRight.png";  // Then set the source
+    
+    // Store the image reference in your model
+    Model.data.character[0].imgRight = cartmanRight;
+    
+    cartmanRight.onload = function() {
+        context.drawImage(
+            cartmanRight,  // Use the image directly
+            Model.data.character[0].x, 
+            Model.data.character[0].y, 
+            Model.data.character[0].cartmanWidth, 
+            Model.data.character[0].cartmanHeight
+        );
+    };
+}
