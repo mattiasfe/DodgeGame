8e16274b0724a62a4a97b8585976a3fb43a04a99
mattiasfe <matferge@gmail.com>
2025-07-01 13:32:50 +0200

    g

--- mainView.js
+++ mainView.js
@@ -30,17 +30,4 @@ function showCharacter() {
     cartmanLeft.src = "CartmanLeft (2).png";
 }
 
-function moveCharacter(e) {
-    if (!Model.data?.character?.[0]) return null;
 
-    switch(e.code) {
-        case "ArrowRight":
-        case "KeyD":
-            return Model.data.character[0].imgRight;
-        case "ArrowLeft":
-        case "KeyA":
-            return Model.data.character[0].imgLeft;
-        default:
-            return null;
-    }
-}
