b2151b1bf1e1e7b812ff865f0ccbdb80feb74aee
mattiasfe <matferge@gmail.com>
2025-07-04 13:04:53 +0200

    gg

--- caracterController.js
+++ caracterController.js
@@ -1,14 +1,11 @@
-function moveCharacter(e) {
-    if (!Model.data?.character?.[0]) return null;
+// function moveCharacter(e) {    
+//     if (e.code == "ArrowLeft" || e.code == "KeyD"){
+//         Model.data.character[0].imgLeft= cartmanLeft;
+//         cartmanLeft.src = "CartmanLeft (2).png";
+//     }
+//     else if(e.code == "ArrowRight" || e.code == "KeyA"){
+//         Model.data.character[0].imgRight= cartmanRight;
+//         cartmanRight.src = "CartmanRight (2).png";
+//     }
 
-    switch(e.code) {
-        case "ArrowRight":
-        case "KeyD":
-            return Model.data.character[0].imgRight;
-        case "ArrowLeft":
-        case "KeyA":
-            return Model.data.character[0].imgLeft;
-        default:
-            return null;
-    }
-}
+// }
--- mainView.js
+++ mainView.js
@@ -1,33 +1,73 @@
 let board;
 let context;
 
+
+function preloadCharacterImages() {
+    Model.data.character[0].imgRight = new Image();
+    Model.data.character[0].imgRight.src = "CartmanRight (2).png";
+    
+    Model.data.character[0].imgLeft = new Image();
+    Model.data.character[0].imgLeft.src = "CartmanLeft (2).png";
+    
+
+    Model.data.character[0].imgDefault = Model.data.character[0].imgRight;
+}
+
 function showBackground() {
     board = document.getElementById("board");
     board.height = Model.data.board.height;
     board.width = Model.data.board.width;
     context = board.getContext("2d");
-    showCharacter();
+    
+
+    drawCharacter('default');
 }
 
-function showCharacter() {
+
+function drawCharacter(direction) {
     
-    let cartmanRight = new Image(); 
-    cartmanRight.src = "CartmanRight (2).png"; 
+    context.clearRect(0, 0, board.width, board.height);
     
-    Model.data.character[0].imgRight = cartmanRight;
-    
-    cartmanRight.onload = function() {
+    let imgToDraw;
+    switch(direction) {
+        case 'left':
+            imgToDraw = Model.data.character[0].imgLeft;
+            break;
+        case 'right':
+            imgToDraw = Model.data.character[0].imgRight;
+            break;
+        default:
+            imgToDraw = Model.data.character[0].imgDefault;
+    }
+
+    if (imgToDraw.complete) {
         context.drawImage(
-            cartmanRight,  
+            imgToDraw,  
             Model.data.character[0].x, 
             Model.data.character[0].y, 
             Model.data.character[0].cartmanWidth, 
             Model.data.character[0].cartmanHeight
         );
-    };
-
-    cartmanLeft = new Image();
-    cartmanLeft.src = "CartmanLeft (2).png";
+    }
 }
 
+function handleKeyPress(e) {
+    if (e.code == "ArrowLeft" || e.code == "KeyA") {
+        drawCharacter('left');
+         Model.data.character[0].x -= 13;
+    }
+    else if (e.code == "ArrowRight" || e.code == "KeyD") {
+        drawCharacter('right');
+         Model.data.character[0].x += 13;
+    }
+}
+
+
+function initGame() {
+    preloadCharacterImages();
+    showBackground();
+    document.addEventListener('keydown', handleKeyPress);
+}
+
+initGame();
 
